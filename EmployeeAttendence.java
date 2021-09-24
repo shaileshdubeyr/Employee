7f42b586dae252281467bdafd20063ebf6bd9ba4
shaileshdubeyr <54022188+shaileshdubeyr@users.noreply.github.com>
2021-09-25 02:14:52 +0530

    Update EmployeeAttendence.java

--- EmployeeAttendence.java
+++ EmployeeAttendence.java
@@ -8,16 +8,16 @@ public class EmployeeAttendence {
 			int FullDay = 8;
 			int TotalSal;
 			System.out.println("Welcome to Employee Wage Computation");
-			int EmpStatus=(int)Math.random() * 2;
+			int EmpStatus = (int)Math.random() * 2;
 			if(EmpStatus == 1)
 			{
 				System.out.println("Employee is present");
-				TotalSal=WagPerHrs * FullDay *EmpStatus;
+				TotalSal= WagPerHrs * FullDay *EmpStatus;
 			}
 			else
 			{
 				System.out.println("Employee is not present");
-				TotalSal= WagPerHrs * FullDay *EmpStatus;
+				TotalSal= WagPerHrs * FullDay * EmpStatus;
 			}
 		System.out.println("The total salary is " +TotalSal);
 	}
